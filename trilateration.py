35a84eac4ca9c88032177d6c7a49f7bffad24f13
NikitaS2001 <nikitasmadych2001@gmail.com>
2023-01-13 13:32:57 +1000

    added trilateration class prototype

--- trilateration.py
+++ trilateration.py
@@ -4,10 +4,15 @@ import scipy
 
 class Trilateration:
 
+    """Solution of the trilateration problem
+    in the form of a system of nonlinear equations
+    using global optimization methods"""
+
     def __init__(self, base_coord :dict) -> None:
-        self.base_coord = base_coord
+        self.__bases_coord = base_coord
+        self.__visible_bases = set(base_coord.keys())
     
-    def solve(distances :numpy.array, guess :numpy.array) -> numpy.array:
+    def solve(self, base_dist:dict, guess :numpy.array) -> numpy.array:
         pass
 
     def __jac(self, x)-> function:
