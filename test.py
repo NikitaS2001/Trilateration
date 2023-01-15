8801f0f2317de85c3203d8c6a949758177780ab1
Никита Смадыч <smadych.ns@students.dvfu.ru>
2023-01-15 16:54:25 +1000

    added full speed test

--- test.py
+++ test.py
@@ -10,10 +10,10 @@ field_z = [0., 5] # field dimensions z
 count_points = 300 # amount of points
 er_range = [-0.5, 0.5] # error range
 
-bases_coord = { 1:[-2. -2, 0],
-                2:[2, -2, 0],
-                3:[2, 2, 0],
-                4:[-2, 2, 0]
+bases_coord = { '1':[-2, -2, 0],
+                '2':[2, -2, 0],
+                '3':[2, 2, 0.2],
+                '4':[-2, 2, 0]
 }
 
 def get_th_points(fx: list, fy: list, fz: list) -> np.array:
@@ -26,17 +26,20 @@ def get_th_points(fx: list, fy: list, fz: list) -> np.array:
     for i, v in enumerate(th):
         result[0][i] = math.atanh(math.cos(v))
         result[1][i] = v
-        result[2][i] = 2
+        result[2][i] = math.cos(v)
     return result
 
-def get_th_dist(theor: np.array, coord: dict) -> np.array:
-    result = np.zeros([count_points, len(coord.keys())])
-    for i, v in enumerate(result):
-        pass
+def get_th_dist(coord: dict, theor: np.array) -> np.array:
+    result = list()
+    for i in range(count_points):
+        d = dict()
+        for v in list(coord.keys()):
+            d[v] = dist(coord.get(v), theor[0][i], theor[1][i], theor[2][i])
+        result.append(d)
     return result
 
-def dist(bases_coord: list, x: float, y: float, z: float) -> float:
-    """ Return float
+def dist(bases_coord: list, x: float, y: float, z: float = 0) -> dict:
+    """ Return dict
 
     Calculation of distance between points
     """
@@ -45,7 +48,36 @@ def dist(bases_coord: list, x: float, y: float, z: float) -> float:
                     + math.pow(z-bases_coord[2], 2))
 
 if __name__ == "__main__":
+    #print(bases_coord)
     th = get_th_points(field_x, field_y, field_z)
+    th_dist = get_th_dist(bases_coord, th)  
 
-    plt.plot(th[0], th[1], ".")
+    cl = np.zeros([3, count_points])
+
+    tril = Trilateration(bases_coord)
+
+    x0 = np.zeros([3])
+
+    for j in range(count_points):
+        tstart = time.time()
+
+        sol = tril.solvelm(th_dist[j], x0)
+        x0 = sol.x
+        cl[0][j] = sol.x[0]
+        cl[1][j] = sol.x[1]
+        cl[2][j] = sol.x[2]
+
+        tstop = time.time()
+        print('Optimize root time', tstop-tstart)
+        tstart = tstop
+
+    fig = plt.figure()
+
+    #plt.plot(cl[0], cl[1], "+")
+    #plt.plot(th[0], th[1], ".")
+    
+    ax = plt.axes(projection='3d')
+    ax.plot3D(th[0], th[1], th[2], ".")
+    ax.plot3D(cl[0], cl[1], cl[2], "+")
+
     plt.show()
--- trilateration.py
+++ trilateration.py
@@ -10,6 +10,8 @@ class Trilateration:
 
     __bases_coord: dict
     __base_dist: dict
+    __tolerance: float = 0.5e-3 # tolerance for termination
+    __iterMax: int = 20 # maximum number of iterations
 
     def __init__(self, base_coord: dict) -> None:
         self.__bases_coord = base_coord
@@ -17,27 +19,46 @@ class Trilateration:
     def set_bases_coord(self, base_coord: dict) -> None:
         self.__bases_coord = base_coord
 
-    def get_bases_coord(self, base_coord: dict) -> dict:
+    def get_bases_coord(self) -> dict:
         return self.__bases_coord
+
+    def set_iterMax(self, iterMax: int) -> None:
+        self.__iterMax = iterMax
+
+    def get_iterMax(self) -> int:
+        return self.__iterMax
 
     def solvelm(self, base_dist :dict, guess: numpy.array) -> numpy.array:
         """ Return a numpy.array [x, y, z]
 
-        Solves the system of nonlinear equations in a least squares sense 
+        Solves the system of non-linear equations in a least squares sense 
         using a modification of the Levenberg-Marquardt algorithm
         """
         self.__base_dist = base_dist
         # return scipy.optimize.root(self.__fun, guess, method="lm").x
-        return scipy.optimize.root(self.__fun, guess, jac=self.__jac, method="lm").x
+        return scipy.optimize.root(self.__fun, guess, method="lm")
+
+    def slovetsls(self, base_dist :dict, guess: numpy.array):
+        """ Return numpy.array [x, y, z] and int
+        
+        Solve a non-linear system using the TSLS+WD (two-step least squares) method
+        """
+        self.__base_dist = base_dist
+        for iter in range(self.__iterMax):
+            jac, fun = self.__jac(guess), self.__fun(guess)
+            if math.sqrt(numpy.dot(fun, fun) / len(guess)) < self.__tolerance:
+                return guess, iter
+            dguess = numpy.linalg.solve(jac, fun)
+            guess = guess - dguess
 
     def __jac(self, v):
         """ Return function
 
         Creation of the Jacobian matrix
         """
-        f = numpy.zeros([len(self.__base_dist.keys()), 3])
-        for i, base in enumerate(self.__base_dist):
-            for j in range(len(self.__bases_coord.values()[0])):
+        f = numpy.zeros([len(self.__base_dist.keys()), len(list(self.__bases_coord.values())[0])])
+        for i, base in enumerate(list(self.__base_dist.keys())):
+            for j in range(len(list(self.__bases_coord.values())[0])):
                 f[i][j] = 2*(v[j]-self.__bases_coord.get(base)[j])
         return f
 
@@ -47,9 +68,8 @@ class Trilateration:
         Creation of a system of nonlinear equations        
         """
         f = numpy.zeros([len(self.__base_dist)])
-        for i, base in enumerate(self.__base_dist):
-            f[i] = math.pow(v[0]-self.__bases_coord.get(base)[0], 2) \
-                 + math.pow(v[1]-self.__bases_coord.get(base)[1], 2) \
-                 + math.pow(v[2]-self.__bases_coord.get(base)[2], 2) \
-                 - math.pow(self.__base_dist.get(base), 2)
+        for i, base in enumerate(list(self.__base_dist.keys())):
+            for j in range(len(list(self.__bases_coord.values())[0])):
+                f[i] += math.pow(v[j]-self.__bases_coord.get(base)[j], 2)
+            f[i] -= math.pow(self.__base_dist.get(base), 2)
         return f
